73ea064208944588240320badc2640aee99b7fad
wanqianxn <ywanqian95@gmail.com>
2016-12-08 12:34:20 -0500

    '3'

--- application.py
+++ application.py
@@ -268,6 +268,6 @@ def writestory():
     return jsonify(result=0)
     
 if __name__ == '__main__':
-    from os import environ
-    app.run(debug=False, port=environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
         
