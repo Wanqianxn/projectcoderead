fd878a3df923c9808b6ae09c274de3f7e0fc22a6
wanqianxn <ywanqian95@gmail.com>
2016-12-08 13:17:26 -0500

    '7'

--- application.py
+++ application.py
@@ -32,7 +32,7 @@ app.config.update(
 )
  
 def make_celery(app):
-    celery = Celery(app.import_name, backend=app.config['CELERY_BACKEND'],
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
                     broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
