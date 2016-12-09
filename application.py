dca8c1f909339b3e74dbadd0fb8e822986ec8e71
wanqianxn <ywanqian95@gmail.com>
2016-12-09 06:15:23 -0500

    'j'

--- application.py
+++ application.py
@@ -36,7 +36,7 @@ q = Queue(connection=conn)
 global thedata
 global hprev
 global txt
-
+app.jinja_env.globals['thedata'] = []
 # General comments: For every page generated, a cleanup function is first executed on GET to clean the system free of uploaded files. For the pages with files to be uploaded, additional code for POST is written to vet those files, make sure they are of the right size before saving them to be processed and outputted.
 
 # Index, Acknowledgments and Contact.         
@@ -258,9 +258,16 @@ def createemma():
 @app.route('/writeupdate')
 def writeupdate():
     d = request.args.get('d')
-    thedata = fixed(d)
-    hprev = numpy.zeros((100,1))
-    thedata[10],thedata[11],thedata[5],thedata[6],thedata[8],thedata[7],thedata[9],thedata[12],thedata[13],thedata[14],thedata[15],thedata[16],thedata[17],hprev,txt = iteration(d,thedata[10],thedata[11],thedata[1],hprev,thedata[3],thedata[17],thedata[4],thedata[2],thedata[5],thedata[6],thedata[8],thedata[7],thedata[9],thedata[12],thedata[13],thedata[14],thedata[15],thedata[16])
+    if app.jinja_env.globals['thedata'] = []:
+        thedata = fixed(d)
+        hprev = numpy.zeros((100,1))
+        thedata.append(hprev)
+        txt = ''
+        thedata.append(txt)
+    else:
+        thedata = app.jinja_env.globals['thedata']
+    thedata[10],thedata[11],thedata[5],thedata[6],thedata[8],thedata[7],thedata[9],thedata[12],thedata[13],thedata[14],thedata[15],thedata[16],thedata[17],thedata[18], thedata[19] = iteration(d,thedata[10],thedata[11],thedata[1],hprev,thedata[3],thedata[17],thedata[4],thedata[2],thedata[5],thedata[6],thedata[8],thedata[7],thedata[9],thedata[12],thedata[13],thedata[14],thedata[15],thedata[16])
+    app.jinja_env.globals['thedata'] = thedata
     return jsonify(result=txt)
     
     
